0e20fb5b562c5b0f5e10fa076d66db063f30331d
Rahul Pandey <therahulogy@gmail.com>
2023-09-05 01:50:43 +0530

    Add files via upload

---
+++ CredentialService.java
@@ -0,0 +1,11 @@
+package GreatLearningExercise;
+
+public interface CredentialService {
+	
+	public String generateEmailAddress(String firstName, String lastName, String department);
+	
+	public String generatePassword();
+	
+	public void showCredentials(Employee employee);
+
+}
